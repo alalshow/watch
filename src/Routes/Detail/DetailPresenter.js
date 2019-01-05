974b59aa1175d588c48efaa2948a63f2252ead15
alalshow <alalshow@hanmail.net>
2019-01-06 05:40:58 +0900

    123

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -5,10 +5,10 @@ import Helmet from "react-helmet";
 import Loader from "Components/Loader";
 
 const Container = styled.div`
-  height: calc(100vh - 50px);
+  height: calc(100vh - 20px);
   width: 100%;
   position: relative;
-  padding: 50px;
+  padding: 20px;
 `;
 
 const Backdrop = styled.div`
@@ -25,16 +25,33 @@ const Backdrop = styled.div`
   z-index: 0;
 `;
 
+const Summary = styled.div`
+  display: flex;
+  width: 100%;
+  position: relative;
+  z-index: 1;
+  height: 20%;
+`;
+
 const Content = styled.div`
   display: flex;
   width: 100%;
   position: relative;
   z-index: 1;
-  height: 100%;
+  height: 35%;
+`;
+
+const Description = styled.div`
+  display: flex;
+  width: 100%;
+  position: relative;
+  z-index: 1;
+  height: 35%;
+  margin-top : 30px;
 `;
 
 const Cover = styled.div`
-  width: 30%;
+  width: 100%;
   background-image: url(${props => props.bgImage});
   background-position: center center;
   background-size: cover;
@@ -43,36 +60,37 @@ const Cover = styled.div`
 `;
 
 const Data = styled.div`
-  width: 70%;
+  width: 100%;
   margin-left: 10px;
 `;
 
 const Title = styled.h3`
   font-size: 32px;
+  margin-bottom:10px;
 `;
 
 const ItemContainer = styled.div`
   margin: 20px 0;
 `;
 
-const Item = styled.span``;
+const Item = styled.div``;
 
 const Divider = styled.span`
-  margin: 0 10px;
+  margin-right: 10px;
 `;
 
 const Overview = styled.p`
   font-size: 12px;
   opacity: 0.7;
   line-height: 1.5;
-  width: 50%;
+  width: 100%;
 `;
 
 const DetailPresenter = ({ result, loading, error }) =>
   loading ? (
     <>
       <Helmet>
-        <title>Loading | Nomflix</title>
+        <title>Loading | Alflix</title>
       </Helmet>
       <Loader />
     </>
@@ -81,38 +99,34 @@ const DetailPresenter = ({ result, loading, error }) =>
       <Helmet>
         <title>
           {result.original_title ? result.original_title : result.original_name}{" "}
-          | Nomflix
+          | Alflix
         </title>
       </Helmet>
       <Backdrop
         bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
       />
-      <Content>
-        <Cover
-          bgImage={
-            result.poster_path
-              ? `https://image.tmdb.org/t/p/original${result.poster_path}`
-              : require("../../assets/noPosterSmall.png")
-          }
-        />
-        <Data>
-          <Title>
-            {result.original_title
-              ? result.original_title
-              : result.original_name}
-          </Title>
+      <Summary>
           <ItemContainer>
+            <Title>
+              {result.original_title
+                ? result.original_title
+                : result.original_name}
+            </Title>
             <Item>
+              Release
+              <Divider>:</Divider>
               {result.release_date
-                ? result.release_date.substring(0, 4)
-                : result.first_air_date.substring(0, 4)}
+                ? result.release_date.substring(0, 7)
+                : result.first_air_date.substring(0, 7)}
             </Item>
-            <Divider>•</Divider>
             <Item>
+              Runtime
+              <Divider>:</Divider>
               {result.runtime ? result.runtime : result.episode_run_time[0]} min
             </Item>
-            <Divider>•</Divider>
             <Item>
+              Genres
+              <Divider>:</Divider>
               {result.genres &&
                 result.genres.map((genre, index) =>
                   index === result.genres.length - 1
@@ -121,9 +135,21 @@ const DetailPresenter = ({ result, loading, error }) =>
                 )}
             </Item>
           </ItemContainer>
+      </Summary>
+      <Content>
+        <Cover
+          bgImage={
+            result.poster_path
+              ? `https://image.tmdb.org/t/p/original${result.poster_path}`
+              : require("../../assets/noPosterSmall.png")
+          }
+        />
+      </Content>
+      <Description>
+        <Data>
           <Overview>{result.overview}</Overview>
         </Data>
-      </Content>
+      </Description>
     </Container>
   );
 
