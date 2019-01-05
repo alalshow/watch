bca1b54661ddc03309eeff2c88d801f448e0e432
alalshow <alalshow@hanmail.net>
2019-01-06 06:03:01 +0900

    Updates

--- precache-manifest.9431b6367b2a80c6519b908aab377a3e.js
+++ precache-manifest.9431b6367b2a80c6519b908aab377a3e.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/watch/static/js/runtime~main.a6d09caf.js"
   },
   {
-    "revision": "e485ed332c1b09444bc5",
-    "url": "/watch/static/js/main.e485ed33.chunk.js"
+    "revision": "6a7118b1793ea7b288c3",
+    "url": "/watch/static/js/main.6a7118b1.chunk.js"
   },
   {
     "revision": "b497b140cc5c53455db8",
     "url": "/watch/static/js/1.b497b140.chunk.js"
   },
   {
-    "revision": "9daab7d593c74abb8a85c8390b181957",
+    "revision": "59fdb42cddfb834f799e6439be02d47f",
     "url": "/watch/index.html"
   }
 ];
